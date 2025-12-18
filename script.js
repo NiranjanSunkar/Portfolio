46177ebacfe181dfb5d303d5763e32b3490d138d
Niranjan Sunkaraboina <niranjansunkaraboina@gmail.com>
2025-12-18 23:37:03 +0530

    Portfolio

---
+++ script.js
@@ -0,0 +1,30 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 150;
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href").includes(current)) {
+      link.classList.add("active");
+    }
+  });
+});
+
+
+const form = document.querySelector(".contact-form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  alert("Thank you! Your message has been sent 🚀");
+  form.reset();
+});
+
